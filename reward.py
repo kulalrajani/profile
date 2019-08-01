4c590e78612d536b3609cdac88515bd773243862
kulalrajani <53164502+kulalrajani@users.noreply.github.com>
2019-08-01 14:15:47 +0530

    Delete reward.py

--- reward.py
+++
@@ -1,9 +0,0 @@
-reward = int(input("Enter your reward points:"))
-if reward >= 0 and reward <=1000:
-    ctype = "SILVER"
-elif reward > 1000 and reward <=10000: 
-    ctype = "GOLD"
-else:
-    ctype = "PLATINUM"
-
-print(f"Welcome {ctype} card holder")    
