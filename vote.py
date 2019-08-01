d252029bfe4e690b14b9bec28a897a2df1e3db84
kulalrajani <53164502+kulalrajani@users.noreply.github.com>
2019-08-01 14:17:23 +0530

    Delete vote.py

--- vote.py
+++
@@ -1,6 +0,0 @@
-"""Program to check the eligibility for voting"""
-age = int(input("Enter your age"))
-if age >= 18:
-    print("Thank you for voting")
-else:
-    print("Sorry..!your not eligible")
