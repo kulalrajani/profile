7ec20ca3d916f75ac46e3523966325edde915e1b
kulalrajani <53164502+kulalrajani@users.noreply.github.com>
2019-08-01 14:17:06 +0530

    Delete sum.py

--- sum.py
+++
@@ -1,7 +0,0 @@
-''' Program to find sum of first n natural number'''
-n = int(input("Enter the value of n:"))
-sum = 0
-for i in range(1,n+1):
-    sum += i
-print(f"Sum of first {n} number is {sum}")
-   
